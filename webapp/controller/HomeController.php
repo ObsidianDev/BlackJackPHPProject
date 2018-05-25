bbef3724f664aa45303d9c8553d65576139dfa64
Afonso Vidal <afonsoxvp@hotmail.com>
2018-05-25 10:41:02 +0100

    Inicio do desenvolvimento do Layout
    Foi removido o conteudo da pasta view/layout para ser criado conteudo proprio. Foi acrescentado os sons e as imagens fornecidos.

--- webapp/controller/HomeController.php
+++ webapp/controller/HomeController.php
@@ -14,45 +14,12 @@ class HomeController extends BaseController
 {
 
     public function index(){
-
         return View::make('home.index');
     }
 
     public function start(){
-
         //View::attachSubView('titlecontainer', 'layout.pagetitle', ['title' => 'Quick Start']);
         return View::make('home.start');
     }
-
-    public function login(){
-        Throw new Exception('Method not implemented. Do it yourself!');
-    }
-
-
-    public function worksheet(){
-
-        View::attachSubView('titlecontainer', 'layout.pagetitle', ['title' => 'MVC Worksheet']);
-
-        return View::make('home.worksheet');
-    }
-
-    public function setsession(){
-        $dataObject = MetaArmCoreModel::getComponents();
-        Session::set('object', $dataObject);
-
-        Redirect::toRoute('home/worksheet');
-    }
-
-    public function showsession(){
-        $res = Session::get('object');
-        var_dump($res);
-    }
-
-    public function destroysession(){
-
-        Session::destroy();
-        Redirect::toRoute('home/worksheet');
-    }
-
 
 }
